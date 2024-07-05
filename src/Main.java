2b681f269dc5d6c172e43b30547b346083ad2b55
Arafat <arafatnounagnonalabi@gmail.com>
2024-07-05 09:21:08 -1200

    last update

--- src/Main.java
+++ src/Main.java
@@ -15,75 +15,55 @@ public static int number;
     public static void main(String[]args)
     {
         starting();
-    First:
-        for (int i = 1; i<4 ;i++) {
-            System.out.print("Minimum Value:");
+
+             System.out.print("Minimum Value:");
             int min = s.nextInt();
-            s.nextLine();
+
             System.out.print("Maximum Value:");
             int max = s.nextInt();
-            s.nextLine();
-            if (range(min,max)) {
 
-                System.out.println("Enter the number of time you wish to play within 5 and 10 ");
+            if ((max-min) >=10) {
+
+                System.out.println("Enter the number of chance to try: ");
                 System.out.print("Number of times: ");
                 int limit = s.nextInt();
-                if (limit<=10 && limit>=5)
+                int i = 0, userInput;
+                do
                 {
-
                     number = guessedNumber(min, max);
-                    for ( int j =1 ;j<=limit;j++)
-                    {
                         System.out.println("Enter the correct number");
-                        int userInput = s.nextInt();
+                         userInput = s.nextInt();
                         if (number<userInput)
                         {
                             System.out.println("Too high");
                         }
-                        else if (number>userInput)
+                         else if (number>userInput)
                         {
                             System.out.println("Too low");
                         }
 
-                        else
+                       else if (userInput == number)
                         {
                             System.out.println(" !!!!!!!!!!!!!!!!!!!! Congratulation !!!!!!!!!!!!!!!! ");
                             System.out.println( "!!!!!!!!!!!!!!!!!!! you win !!!!!!!!!!!!!!!!!!! ");
-                        break First;
+                            return;
                         }
-                        System.out.println(limit - j + " test(s) remain");
 
-                          if( j >= limit)
-                    {
-                        System.out.println("!!!You loose!!!");
-                        break First;
-                    }
-                    }
+                       i++;
+                }
+                   while(i<limit);
+                if (userInput!= number)
+                {
+                    System.out.println("!!!You loose!!!");
+                    System.out.println("The correct number is: "+number);
                 }
 
-                System.out.println(" !!!Wrong input!!! ");
-                System.out.println((3 - i) + " test(s) remain");
-
-
-
-            } else {
-
-                    System.out.println((3 - i) + " test(s) remain");
-                    System.out.println(" ******* NB:Make sure the range between Min and Max value is 10 ******* ");
-            }
-
-        }
-        System.out.println("The correct number is: "+number);
+                }
     }
-public static boolean range(int min,int max)
 
-{
-    return ((max-min) >=10);
-
-}
 public static void starting ()
 {
-    System.out.println("Starting....");
+    System.out.println("Starting ....");
     System.out.println("Guess the correct number");
     System.out.println(" ******* NB:Make sure the range between Min and Max value is not less than 10 ******* ");
 
